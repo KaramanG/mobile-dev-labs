d49a9ef022876fec078d9007c0dbe020c8c1b5ce
KaramanG <karaman.gabriel1@gmail.com>
2025-12-28 19:35:07 +0300

    Загрузка первой практики

---
+++ Lesson1/buttonclicker/src/main/java/ru/mirea/karamangyu/buttonclicker/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.mirea.karamangyu.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку: 12");
+                checkBox.setChecked(!checkBox.isChecked());
+            }
+        };
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+    public void onMyButtonClick(View v) {
+        Toast.makeText(
+                this,
+                "Второй способ",
+                Toast.LENGTH_SHORT
+        ).show();
+
+        tvOut.setText("Это не я сделал)");
+        checkBox.setChecked(!checkBox.isChecked());
+    }
+}
